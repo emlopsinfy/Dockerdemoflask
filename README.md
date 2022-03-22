29cdc3e5d03f1094e1e9e53d9743bf4a7fb1a922
raajeshlr <raajeshlr2@gmail.com>
2022-03-22 11:16:58 +0530

    dockerdemoflask

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 this is the code for my medium article
 https://medium.com/@aknsmb/running-a-flask-app-in-a-docker-containers-with-vs-code-docker-extension-6e6a2460fcd7
+
+docker build . -t dockerdemoflask:latest
+
+docker run -p 5000:5000 dockerdemoflask:latest
+
+The flask app will not be running on docker build, only while you do docker run, it will host the app.
--- server.py
+++ server.py
@@ -2,10 +2,20 @@ from flask import render_template,request,Flask,redirect
 
 app = Flask(__name__,template_folder='templates')
 
+
 @app.route('/')
 def index():
     return render_template('hello.html')
 
+@app.route('/results')
+def resultfunction():
+    results = {'Accuracy Percentage': 90,'f1score Percentage': 81}
+    return results
+
+print("Hello World")
+    
 if __name__=="__main__":
     app.debug = True
     app.run(host='0.0.0.0')
+
+
